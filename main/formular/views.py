da81666d358086d2f9b8349a2c91442aa1694c8c
Alex <sasha6414@yahoo.com>
2020-08-10 16:53:00 +0300

    URL settings. 1.template formular

--- main/formular/views.py
+++ main/formular/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 # Create your views here.
+
+def formular(request):
+    return render(request, 'form_template.html')
+
+
+
